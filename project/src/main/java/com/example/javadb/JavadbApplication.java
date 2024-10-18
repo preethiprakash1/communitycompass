e33170fb7a7809c6434f9a0e29c1cfee2e4f221f
Divya Tadimeti <dtadimeti@berkeley.edu>
2024-10-18 19:41:43 -0400

    resolving last checkstyle bug

--- project/src/main/java/com/example/javadb/JavadbApplication.java
+++ project/src/main/java/com/example/javadb/JavadbApplication.java
@@ -6,20 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Entry point for the application.
  */
+
 @SpringBootApplication
 public class JavadbApplication {
-
-    // Private constructor to prevent instantiation
-    private JavadbApplication() {
-        throw new UnsupportedOperationException(
-                "This class cannot be instantiated.");
-    }
-
     /**
      * Main method to run the application.
      *
      * @param args the command line arguments
-     *             (must be final according to style check)
      */
     public static void main(final String[] args) {
         SpringApplication.run(JavadbApplication.class, args);
