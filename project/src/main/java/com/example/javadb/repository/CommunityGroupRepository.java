b712521636799c0694b4b2c0909a9ff4034a5a4b
preethiprakash1 <pp2769@columbia.edu>
2024-10-22 03:19:25 +0000

    Fixed issues with database connectivity. Created functions to filter resources and community groups based on type.

--- project/src/main/java/com/example/javadb/repository/CommunityGroupRepository.java
+++ project/src/main/java/com/example/javadb/repository/CommunityGroupRepository.java
@@ -5,9 +5,14 @@
 package com.example.javadb.repository;
 
 import com.example.javadb.model.CommunityGroup;
+import com.example.javadb.model.CommunityGroup.CommunityType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CommunityGroupRepository
         extends JpaRepository<CommunityGroup, Integer> {
-    }
+
+  List<CommunityGroup> findByCommunityType(CommunityType type);
+
+}
 
